9cf5bf9fa626404b19c4f9010bd9576c2850b2ff
nalbam <me@nalbam.com>
2019-09-09 10:38:15 +0900

    aws_ami filter

--- modules/gitlab/15-data.tf
+++ modules/gitlab/15-data.tf
@@ -6,7 +6,22 @@ data "aws_ami" "this" {
   owners = ["782774275127"] # GitLab's account ID.
 
   filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
     name   = "name"
-    values = ["GitLab CE-${var.gitlab_version}.*"]
+    values = ["GitLab CE ${var.gitlab_version}.*"]
   }
 }
